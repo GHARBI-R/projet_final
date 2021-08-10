a450ac3943d67ffc93df3bb1f36c317d0feb0166
GHARBI-R <62842493+GHARBI-R@users.noreply.github.com>
2021-08-10 17:29:04 +0100

    first commit

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -2,13 +2,12 @@
 from odoo import http
 from odoo.http import request
 
-#creation d'un simple controlleur
+
 class Topnet(http.Controller):
 
     @http.route('/utilisateur/list/', website=True, auth='public')
     def topnet_user(self, **kw):
          # return "Thanks for watching"
-
         utilisateurs = request.env['topnet.user'].sudo().search([])
         return request.render("pfe_topnet.user_list", {'utilisateurs': utilisateurs})
 
@@ -16,3 +15,13 @@ class Topnet(http.Controller):
     @http.route('/adduser_form', website='true', auth='user')
     def index(self, **kw):
         return http.request.render("pfe_topnet.create_user", {})
+
+class Topnet(http.Controller):
+    @http.route('/user-success', website='true', auth='user')
+    def index(self, **kw):
+        return http.request.render("pfe_topnet.user_thanks", {})
+
+    class Topnet(http.Controller):
+        @http.route('/user/list', website='true', auth='user')
+        def index(self, **kw):
+            return http.request.render("pfe_topnet.user_list", {})
